50b33043e0074714828c89029c72f165769502ef
maki <mamajohnjohn@gmail.com>
2022-03-21 14:59:54 -0700

    assignment submit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const $ = (id) => document.getElementById(id);
+
+let empForm = $('empForm');
+let id = $('id');
+let name = $('name');
+let ext = $('ext');
+let email = $('email');
+let department = $('department');
+let btnAdd = $('btnAdd');
+
+empForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(`ID: ${id.value}`);
+    console.log(`Name: ${name.value}`);
+    console.log(`Extension: ${ext.value}`);
+    console.log(`Email: ${email.value}`);
+    console.log(`Department: ${department.value}`);
+})
